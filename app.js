11b9efe13a73219f95e3ee3f09767f78a667772f
maxawolff <maxawolff@hotmail.com>
2017-08-21 14:52:40 -0700

    added event listener to track clicks

--- app.js
+++ app.js
@@ -7,10 +7,31 @@ function Image(name, path){
   this.timesClicked = 0;
 }
 
-var bag = new Image(bag, 'img/bag.jpg');
+var bag = new Image('bag', 'img/bag.jpg');
+var ban = new Image('banana', 'img/banana.jpg');
+var bath = new Image('bathroom', 'img/bathroom.jpg');
+
+var position = document.getElementsByTagName('body')[0];
 
 var pic1 = document.createElement('img');
-pic1.setAttribute('src', bag.path);
-//pic1.setAttribute('src', 'img/bag.jpg');
-var position = document.getElementsByTagName('body')[0];
+pic1.setAttribute('src', 'img/bag.jpg');
+pic1.className = bag.name;
 position.appendChild(pic1);
+
+var pic2 = document.createElement('img');
+pic2.setAttribute('src', ban.path);
+pic2.className = ban.name;
+position.appendChild(pic2);
+
+var pic3 = document.createElement('img');
+pic3.setAttribute('src', bath.path);
+pic3.className = bath.name;
+position.appendChild(pic3);
+
+document.getElementsByClassName('bag')[0].addEventListener('click', vote);
+
+function vote(event){
+  event.preventDefault();
+  bag.timesClicked ++;
+  console.log('you clicked on the bag, timesClicked = ' + bag.timesClicked);
+}
