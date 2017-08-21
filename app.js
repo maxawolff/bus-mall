5922b1cd24ac56f40472c7c7423ce9867f2530f1
maxawolff <maxawolff@hotmail.com>
2017-08-21 13:48:08 -0700

    added a constructor function and was able to render an image on the page using this

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function Image(name, path){
+  this.name = name;
+  this.path = path;
+  this.timesDisplayed = 0;
+  this.timesClicked = 0;
+}
+
+var bag = new Image(bag, 'img/bag.jpg');
+
+var pic1 = document.createElement('img');
+pic1.setAttribute('src', bag.path);
+//pic1.setAttribute('src', 'img/bag.jpg');
+var position = document.getElementsByTagName('body')[0];
+position.appendChild(pic1);
