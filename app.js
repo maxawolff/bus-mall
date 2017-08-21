d4694c9e7c7e5829c776d78b28f80646e825a87b
maxawolff <maxawolff@hotmail.com>
2017-08-21 15:12:46 -0700

    resized images and made each image into an object

--- app.js
+++ app.js
@@ -8,8 +8,25 @@ function Image(name, path){
 }
 
 var bag = new Image('bag', 'img/bag.jpg');
-var ban = new Image('banana', 'img/banana.jpg');
-var bath = new Image('bathroom', 'img/bathroom.jpg');
+var banana = new Image('banana', 'img/banana.jpg');
+var bathroom = new Image('bathroom', 'img/bathroom.jpg');
+var boots = new Image('boots', 'img/boots.jpg');
+var breakfast = new Image('breakfast', 'img/breakfast.jpg');
+var bubblegum = new Image('bubblegum', 'img/bubblegum.jpg');
+var chair = new Image('chair', 'img/char.jpg');
+var cthulhu = new Image('cthulhu', 'img/cthulhu.jpg');
+var dogDuck = new Image('dogDuck', 'img/dog-duck.jpg');
+var dragon = new Image('dragon', 'img/dragon.jpg');
+var pen = new Image('pen', 'img/pen.jpg');
+var petSweep = new Image('petSweep', 'img/pet-sweep.jpg');
+var scissors = new Image('scissors', 'img/scissors.jpg');
+var shark = new Image('shark', 'img/shark.jpg');
+var sweep = new Image('sweep', 'img/sweep.png');
+var tauntaun = new Image('tauntaun', 'img/tauntaun.jpg');
+var unicorn = new Image('unicorn', 'img/unicorn.jpg');
+var usb = new Image('sub', 'img/usb.gif');
+var waterCan = new Image('waterCan', 'img/water-can.jpg');
+var wineGlass = new Image('wineGlass', 'img/wine-glass.jpg');
 
 var position = document.getElementsByTagName('body')[0];
 
