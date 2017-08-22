90a4507a02ce1ffe5e5656e1f068429d2ea40278
maxawolff <maxawolff@hotmail.com>
2017-08-22 14:08:51 -0700

    results are now displayed as a bar chart showing the number of times an image has been clicked on

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ var prevIndex2;
 var prevIndex3;
 // use an array for indicies
 var timesVoted = 0;
+var clickResults = [];
+var displayResults = [];
+var imageNames = [];
 
 function Image(name, path){
   this.name = name;
   this.path = path;
   this.timesDisplayed = 0;
   this.timesClicked = 0;
+  imageNames.push(name);
 }
 
 // use a for loop to create these, no need to give them a name since they are stored in an array, not needed just a though
@@ -132,14 +136,43 @@ function vote3(event){
 // one function and find what you clicked by checking its id and comparing it to the id of what was clikced on
 
 var votingResults = function(){
-  var list = document.createElement('ul');
-  position.appendChild(list);
-
   for(var i = 0; i < images.length; i ++){
-    var li = document.createElement('li');
-    list.appendChild(li);
-    li.innerText = images[i].name + ' was seen ' + images[i].timesDisplayed + ' times, and voted for ' + images[i].timesClicked + ' times';
+    displayResults.push(images[i].timesDisplayed);
+    clickResults.push(images[i].timesClicked);
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var chartConfig = {
+      type: 'bar',
+      data: {
+        labels: imageNames, // x-axis labels for every entry in your data set. It should match up with the number of things you're plotting (if it's a bar chart)
+        datasets: [{ // <-- notice that this can be an array of multiple data sets.
+          // each data set is its own object literal.
+          label: '# of Votes', // <-- the label of this one data set
+          data: clickResults, // <-- where your data actually goes. just the numbers
+          backgroundColor: [ // <-- this can be either one single color or a color for each item in your bar chart.
+            'rgba(255, 99, 132, 0.2)',
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+          ],
+          borderWidth: 5
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+    };
+    var myChart = new Chart(ctx, chartConfig);
+
   }
 };
 
 displayImages();
+var test = [2, 12, 6, 7, 18, 3, 10];
